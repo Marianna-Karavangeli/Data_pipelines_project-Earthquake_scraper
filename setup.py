95a6399fcd15ca99188e8f1a4b536dd16d732a4e
Marianna Karavangeli <mariannakaravageli@yahoo.com>
2021-08-31 14:19:39 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 from setuptools import find_packages
+from pathlib import Path 
+
+this_directory = Path(__file__).parent 
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='earthquakescraper', ## This will be the name your package will be published with
     version='0.0.2', 
     description='Web scraper that scrapes erathquake data from the USGS.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/Marianna-Karavangeli/Earthquake_scraper.git', # Add the URL of your github repo if published 
                                                                    # in GitHub
     author='Marianna-Karavangeli', # Your name
